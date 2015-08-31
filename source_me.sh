133d46bfc3dc28a5143cdf20677dcbbf393a92c8
Gil Vegliach <gil.vegliach@gmail.com>
2015-08-31 12:41:13 +0200

    Fix permissions and handle empty directories

--- source_me.sh
+++ source_me.sh
@@ -9,13 +9,18 @@
 # parameters and then performs the brace expansion during the eval.
 # The results are put in arrays to make it easier to loop through
 
+# Remember if nullglob is enabled (zero)
+shopt -q nullglob
+is_nullglob_enabled=$?
+
+# Nullglob: filename patterns matching no files expand to a null 
+# string, rather than to the patterns themselves
+shopt -s nullglob
+
 root="~/Scripts"
 functs=( `eval echo "${root}/{public,private}/functions"` )
 aliases=( `eval echo "${root}/{public,private}/aliases"` ) 
 execs=( `eval echo "${root}/{public,private}/executables"` ) 
-
-# Termporary, remove
-source ~/Scripts/private/aliases/zalando
 
 for fun in "${functs[@]}"; do
     if [ -d $fun ]; then
@@ -37,10 +42,15 @@ for exc in "${execs[@]}"; do
    export PATH=$PATH:"$exc"
 done
 
+# Restore nullglob
+if [ $is_nullglob_enabled -ne 0 ]; then
+    shopt -u nullglob
+fi
+
+unset is_null_glob
 unset fun
 unset als
 unset exc
-
 unset root
 unset functs
 unset execs
