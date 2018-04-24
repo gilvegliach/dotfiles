4f4d58fba8ab3d1083434f5a5a817f7696124d05
Gil Vegliach <gil.vegliach@gmail.com>
2018-04-24 22:46:57 +0100

    Unify bash history through sessions

--- public/sourced/main.sh
+++ public/sourced/main.sh
@@ -1,6 +1,10 @@
 # The intent of this file is to collect common public settings that usually
 # go in .profile
 
+# unified bash history
+shopt -s histappend
+PROMPT_COMMAND="${PROMPT_COMMAND:+$PROMPT_COMMAND$'\n'}history -a; history -c; history -r"
+
 [ -f ~/.fzf.bash ] && source ~/.fzf.bash
 export FZF_DEFAULT_OPTS="--no-color"
 
